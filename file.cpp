96758f72391fe39f0dd44e785af2398eb7a8ba19
Utkrishta Sinha <krishsinha2406@gmail.com>
2022-01-16 11:20:03 +0530

    add

--- file.cpp
+++ file.cpp
@@ -1,31 +1,87 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long int
-#include<math.h>
-int main() {
-    // your code goes here
-    int t;
-    cin>>t;
-    while(t--)
-    {
-       ll n,k=0;
-       cin>>n;
-       n-=1;
-        for (int i=1; i<=sqrt(n); i++)
-    {
-        if (n%i == 0)
-        {
-            // If divisors are equal, print only one
-            if (n/i == i)
-               k+=1;
-  
-            else // Otherwise print both
-                k+=2;
-        }
-    }
-       
-       cout<<k<<endl;
-      
-    }
-    return 0;
+
+int main()
+{
+       int t;
+       cin >> t;
+       while (t--)
+       {
+              int n;
+              cin>>n;
+              int red[3];
+              for(int i=0;i<3;i++)
+              {
+                     cin>>red[i];
+              }
+              int green[3];
+              for(int i=0;i<3;i++)
+              {
+                     cin>>green[i];
+              }
+              int blue[3];
+              for(int i=0;i<3;i++)
+              {
+                     cin>> blue[i];
+              }
+              int ans=0; 
+              if(red[0]==n && green[1]==n && blue[2]==n)
+              {
+                     ans=0;
+              }
+              while(red[0]!=n)
+              {
+                     if(blue[0]!=0)
+                     {
+                            red[0]++;
+                            red[2]--;
+                            blue[2]++;
+                            blue[0]--;
+                            ans++;
+                            
+                     }
+                     if(green[0]!=0)
+                     {
+                            red[0]++;
+                            red[1]--;
+                            green[1]++;
+                            green[0]--;
+                            ans++;
+                     }
+
+
+
+              }
+              while(blue[2]!=n)
+              {
+                     if(red[2]!=0)
+                     {
+                            red[0]++;
+                            red[2]--;
+                            blue[2]++;
+                            blue[0]--;
+                            ans++;
+                            
+                     }
+                     if(green[2]!=0)
+                     {
+                            blue[2]++;
+                            blue[1]--;
+                            green[2]++;
+                            green[1]--;
+                            ans++;
+                     }
+
+
+
+              }
+
+
+              cout<<ans<<"\n";
+              
+
+              
+       }
+       return 0;
 }
+
